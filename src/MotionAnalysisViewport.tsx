89eee91c6d932e091c1348ad99072875fd5b3c86
mvch1ne <sabithadidariff@gmail.com>
2026-02-19 14:40:09 +0000

    Created Layout

--- src/MotionAnalysisViewport.tsx
+++ src/MotionAnalysisViewport.tsx
@@ -1,7 +1,14 @@
 import './MotionAnalysisViewport.css';
+import video from '../assets/Jonielle Smith 60m.mp4';
 
 function MotionAnalysisViewport() {
-  return <></>;
+  return (
+    <div className="container">
+      <section className="top-bar"></section>
+      <main className="main-video-viewport"></main>
+      <section className="controls-section"></section>
+    </div>
+  );
 }
 
 export default MotionAnalysisViewport;
